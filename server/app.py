471f6201c5841b61cd1ad720f96d8623c77a744c
William Wong <william.wai.lim.wong@gmail.com>
2023-09-05 22:28:12 -0400

    passes all tests

--- server/app.py
+++ server/app.py
@@ -47,6 +47,41 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+
+        record = Plant.query.filter(Plant.id == id).first()
+        patch_json = request.get_json()
+        for attr in patch_json:
+            setattr(record, attr, patch_json[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": ""}
+
+        response = make_response(
+            response_dict,
+            204
+        )
+
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
